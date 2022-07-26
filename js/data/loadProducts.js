28a1dbf21d14236d13a71f62dd4aef47827d9d0b
Daniel Quintero <daniel.manny10@hotmail.com>
2022-07-25 21:53:54 -0500

    fix some issues and add store section

---
+++ js/data/loadProducts.js
@@ -0,0 +1,210 @@
+let products=[
+    {
+        "modelo":"A01 versión M",
+        "marca":"Samsung",
+        "descripcion":"Modulo para Samsung A01 A015M.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4828-modulo-samsung-a01-a015m-negro-original-11-a4fd867d98781d67d116210907082254-640-0.jpg"
+    },
+    {
+        "modelo":"A01 Core",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A01 Core A013.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4902-modulo-samsung-a01-core-a113-negro_11-77fbe43a5b8c52ad5316268855167754-640-0.jpg"
+    },
+    {
+        "modelo":"A02",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A02 A022.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/5102-modulo-samsung-a02-a022-negro-21-04fac6e1d4cd10111616343310135955-640-0.jpg"
+    },
+    {
+        "modelo":"A10",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A10 A105 M10 M105.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4128-modulo-samsung-a10-a105-original_negro1-75ba703e8601c4a87815662378755809-640-0.jpg"
+    },
+    {
+        "modelo":"A11",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A11 A115.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4903-modulo-samsung-a11-a115-negro_11-71648987b2cd976ddf16209368852698-640-0.jpg"
+    },
+    {
+        "modelo":"A20 Oled",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A20 A205.",
+        "calidad":"Oled (big screen) - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4312-modulo-samsung-a20-a205-negro-small-size-oled-regula-brillo1-476591c37e97fb8eaa16002795580574-640-0.jpg"
+    },
+    {
+        "modelo":"A21",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A21S A217.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4829-modulo-sams-a21s-negro-org-a217_11-1e32f61010cb300cc316097872640025-640-0.jpg"
+    },
+    {
+        "modelo":"A30 Oled",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A30 A305.",
+        "calidad":"Oled - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/356/280/products/4312-modulo-samsung-a20-a205-negro-small-size-oled-regula-brillo1-476591c37e97fb8eaa16002791546698-640-0.jpg"
+    },
+    {
+        "modelo":"A30s Oled",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A30s A307.",
+        "calidad":"Oled - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4329-modulo-samsung-a30s-a307-negro-original1-572bd4c05f968c419f16002797571391-640-0.jpg"
+    },
+    {
+        "modelo":"A31 Oled",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A31 A315.",
+        "calidad":"Oled - Con marco",
+        "precio":"12.99",
+        "imagen":"https://parteselectronicas.com/wp-content/uploads/2021/01/Modulo-Samsung-A31-Negro-Oled.jpg"
+    },
+    {
+        "modelo":"A50 Oled",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung A50 A505.",
+        "calidad":"Oled - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://i38.psgsm.net/all-spares.com/p/894055/480/lcd-compatible-with-samsung-a505-galaxy-a50-black-with-touchscreen-oled-high-copy.webp"
+    },
+    {
+        "modelo":"J4 plus * J4 Core * J6 Plus",
+        "marca":"Samsung",
+        "descripcion":"Módulo para Samsung J4Core J4+ J6+ J410 J415 J610.",
+        "calidad":"Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/3948-modulo-samsung-j4-core-j4-j6-j410-j415-j610-oled_negro-4ed1-361741fd7caf50b32015931827655591-640-0.jpg"
+    },
+    {
+        "modelo":"Moto G4",
+        "marca":"Motorola",
+        "descripcion":"Módulo para Motorola Moto G4 XT1621 XT1622 XT1625.",
+        "calidad":"Original - Con marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4058-modulo-motorola-moto-g4-xt1621-xt1622-xt1625-original_negro1-aa2e68368a4defc35415931749784579-640-0.jpg"
+    },
+    {
+        "modelo":"Moto G4 Play",
+        "marca":"Motorola",
+        "descripcion":"Módulo para Motorola Moto G4 Play XT1601 XT1602 XT1603.",
+        "calidad":"Original - Con marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/modulo-motorola-moto-g4-play-xt1601-xt1602-xt1603-original1-b942f9a49a1686925515131084139517-640-0.jpg"
+    },
+    {
+        "modelo":"Moto G6",
+        "marca":"Motorola",
+        "descripcion":"Módulo para Motorola Moto G6 XT1925.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4061-modulo-motorola-moto-g6-xt1925-negro-original1-625185fb5d61623cb415931752524125-640-0.jpg"
+    },
+    {
+        "modelo":"Moto G7 * G7 Plus",
+        "marca":"Motorola",
+        "descripcion":"Módulo para Motorola Moto G7 XT1962.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/3996-modulo-motorola-moto-g7-xt1962_negro-21-3f51987e135d92f90615931753820172-640-0.jpg"
+    },
+    {
+        "modelo":"Moto G7 Power",
+        "marca":"Motorola",
+        "descripcion":"Módulo para Motorola Moto G7 Power XT1955.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/3995-modulo-motorola-moto-g7-power-xt1955_negro-21-534c85ce298e1fd45515931753492351-640-0.jpg"
+    },
+    {
+        "modelo":"Moto G8 Power",
+        "marca":"Motorola",
+        "descripcion":"Módulo para Motorola Moto G8 Power XT2041.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4548-modulo-motorola-moto-g8-power-xt2041-negro1-ee72cded7e53899b3916002776916163-640-0.jpg"
+    },
+    {
+        "modelo":"Moto G8 Play One Macro",
+        "marca":"Motorola",
+        "descripcion":"Módulo para Motorola Moto G8 Play XT2015 One Macro XT2016.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4324-modulo-motorola-moto-g8-play-xt2015-negro-11-72c8e8b699623d5f5f16002731135697-640-0.jpg"
+    },
+    {
+        "modelo":"Redmi Note 6 y 6 Pro",
+        "marca":"Xiaomi",
+        "descripcion":"Módulo para Xiaomi Redmi Note 6 * Note 6 Pro.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/3804_modulo-xiaomi-redmi-note-6-pro_negro-11-8206a91991a3357bf215931927379615-640-0.png"
+    },
+    {
+        "modelo":"Redmi Note 7",
+        "marca":"Xiaomi",
+        "descripcion":"Módulo para Xiaomi Redmi Note 7 * Note 7 Pro.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4038-modulo-xiaomi-redmi-note-7_negro1-06f84daf7e537dd87415931927587544-640-0.jpg"
+    },
+    {
+        "modelo":"Redmi Note 8",
+        "marca":"Xiaomi",
+        "descripcion":"Módulo para Xiaomi Redmi Note 8",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4336-modulo-xiaomi-redmi-note-8-negro1-23afc03e97f25633b816167819813484-640-0.jpg"
+    },
+    {
+        "modelo":"Redmi Note 8 Pro",
+        "marca":"Xiaomi",
+        "descripcion":"Módulo para Xiaomi Redmi Note 8 Pro",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/5104-modulo-xiaomi-redmi-note-8-pro-negro-11-83e05a3d0b93eced1016343315828012-640-0.jpg"
+    }
+]
+
+let getData=async () => {
+    let productsContainer=document.getElementById("products");
+    products.map((data)=>{
+    let card=document.createElement("div")
+    card.classList.add("card")
+    card.classList.add("product")
+    card.innerHTML=`
+        <div class=" h-100 d-flex flex-column align-items-center justify-content-evenly">
+        <div class="ContenedorImagen">
+        <img class="img-fluid" style="height: 250px; width: 250px;" src="${data.imagen}">
+        </div>
+            <h2 class="card-title ">${data.modelo}</h2>
+            <h6 class="card-text align-self-start">${data.marca}</h6>
+            <p class="card-text text-justify">${data.descripcion}</p>
+            <p class="card-text text-center">${data.precio}$</p>
+            <a class="btn btnComprar" href="https://wa.link/tspqjd" target="_blank">Comprar</a>
+        </div>
+    `
+    productsContainer.appendChild(card)
+    })
+}
+
+  getData(products)
---
+++ js/main.js
@@ -0,0 +1,67 @@
+    
+    $(document).ready(function(){
+    $('.slideShow').slick({
+        dots: true,
+        autoplay: false,
+    });
+  });
+
+  let products=[
+    {
+        "modelo":"A01 versión M",
+        "marca":"Samsung",
+        "descripcion":"Modulo para Samsung A01 A015M.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4828-modulo-samsung-a01-a015m-negro-original-11-a4fd867d98781d67d116210907082254-640-0.jpg"
+    },
+
+    {
+        "modelo":"Redmi Note 7",
+        "marca":"Xiaomi",
+        "descripcion":"Módulo para Xiaomi Redmi Note 7 * Note 7 Pro.",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4038-modulo-xiaomi-redmi-note-7_negro1-06f84daf7e537dd87415931927587544-640-0.jpg"
+    },
+    {
+        "modelo":"Redmi Note 8",
+        "marca":"Xiaomi",
+        "descripcion":"Módulo para Xiaomi Redmi Note 8",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/4336-modulo-xiaomi-redmi-note-8-negro1-23afc03e97f25633b816167819813484-640-0.jpg"
+    },
+    {
+        "modelo":"Redmi Note 8 Pro",
+        "marca":"Xiaomi",
+        "descripcion":"Módulo para Xiaomi Redmi Note 8 Pro",
+        "calidad":"Original - Sin marco",
+        "precio":"12.99",
+        "imagen":"https://d3ugyf2ht6aenh.cloudfront.net/stores/288/942/products/5104-modulo-xiaomi-redmi-note-8-pro-negro-11-83e05a3d0b93eced1016343315828012-640-0.jpg"
+    }
+]
+
+let getData=async () => {
+    let productsContainer=document.getElementById("destacadosSection");
+    products.map((data)=>{
+    let card=document.createElement("div")
+    card.classList.add("card")
+    card.classList.add("p-1")
+    card.setAttribute("style","width: 80rem; min-height: 32rem;")
+    card.innerHTML=`
+        <div class=" card-body d-flex flex-column align-items-center justify-content-center ">
+        <div class="ContenedorImagen">
+        <img class="img-fluid" style="height: 250px; width: 250px;" src="${data.imagen}">
+        </div>
+            <h2 class="card-title bold mt-3 ">${data.modelo}</h2>
+            <h6 class="card-text align-self-start">${data.marca}</h6>
+            <p class="card-text text-justify regular">${data.descripcion}</p>
+            <p class="card-text text-center regular">${data.precio}$</p>
+        </div>
+    `
+    productsContainer.appendChild(card)
+    })
+}
+
+  getData(products)
